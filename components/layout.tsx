6333448c8f1bf99217a3ca291ecbc0b1a04a7cd7
Andrew Osei-Wusu <andrew@rootsdigital.tech>
2021-07-08 20:18:53 +0000

    Additions added: google tag manager

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,10 +4,25 @@ import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 import { ILayoutProps } from '../types'
 import ChatUs from '../components/chat'
+import { useEffect } from 'react'
 
 const Layout: FC<ILayoutProps> = ({ pagename, children, navVariant }) => {
   // console.log(userDetails)
   // console.log("data in layout")
+
+  useEffect(() => {
+    if (process.browser) {
+      console.log('adding g-tag')
+      window.dataLayer = window.dataLayer || []
+      function gtag() {
+        dataLayer.push(arguments)
+      }
+      gtag('js', new Date())
+
+      gtag('config', 'G-BPXLJF62DP')
+      console.log(gtag, 'added g-tag')
+    }
+  }, [])
 
   return (
     <>
@@ -23,6 +38,9 @@ const Layout: FC<ILayoutProps> = ({ pagename, children, navVariant }) => {
           </div>
         </div>
       </div>
+
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-BPXLJF62DP"></script>
     </>
   )
 }
